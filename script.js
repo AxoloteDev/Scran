196c317cd806dc89aca7a60f03b61f83722440e9
Renan Kalu <88622721+AxoloteDev@users.noreply.github.com>
2022-11-07 23:30:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Menu
+
+function abrirMenu() {
+	document.getElementById("navegação").style.width = "100%";
+}
+
+function fecharMenu() {
+	document.getElementById("navegação").style.width = "0%";
+}
